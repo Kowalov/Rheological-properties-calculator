9107607219f2843b52653b35321b55827a25e8f1
Piotr Kowalewski <54917696+Kowalov@users.noreply.github.com>
2021-03-15 23:22:56 +0100

    Update settings.py

--- reologia/reologia/settings.py
+++ reologia/reologia/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ky0#&_*#-%!e^r%yw!&#b60a8m(6_d*1ay9b3h^fvxa0j(#gy#'
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
